0e1263b2b5a4fc71dc2eb75674e03c0c4116dc7b
sanskriti480 <sanskriti.ten@gmail.com>
2025-07-23 20:25:00 +0530

    Update and rename Exp-1 Hello world and calculator.cpp to Exp-1 Hello world.cpp

---
+++ Exp-1 Hello world.cpp
@@ -0,0 +1,21 @@
+//Sanskriti Khairnar
+//PRN-24070123054
+//Batch-A2
+//Experiment 1
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    cout<<"Hello World";
+
+    return 0;
+}
+
+//Output
+
+//Hello World
+
+
+
