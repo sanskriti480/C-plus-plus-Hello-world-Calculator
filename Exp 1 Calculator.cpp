06a29cf90a9482724aef892f9d93ae64c71ee88f
sanskriti480 <sanskriti.ten@gmail.com>
2025-07-23 20:24:50 +0530

    Create Exp 1 Calculator.cpp

---
+++ Exp 1 Calculator.cpp
@@ -0,0 +1,38 @@
+//Sanskriti Khairnar
+//PRN-24070123054
+//Batch-A2
+//Experiment 1
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    float num1, num2, sum, div, sub, multiply;
+    cout <<"Enter first number: ";
+    cin >> num1;
+    cout <<"Enter second number: ";
+    cin >> num2;
+    
+    sum = num1 + num2;
+    cout << "sum = " << sum <<endl;
+    
+    sub = num1 - num2;
+    cout << "sub = " << sub <<endl;
+    
+    multiply = num1 * num2;
+    cout << "multiply = " << multiply <<endl;
+    
+    div = num1 / num2;
+    cout << "div = " << div <<endl;
+
+return 0;
+}
+
+//Output
+
+//Enter first number: 80
+//Enter second number: 40
+//sum = 120
+//sub = 40
+//multiply = 3200
+//div = 2
